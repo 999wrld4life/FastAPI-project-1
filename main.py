5397340ce4489661fd15ab84be6ae91b79019e98
Nathaniel Abayneh <natnaelabayneh5@gmail.com>
2024-12-22 22:28:00 +0300

    update post + error handling

--- main.py
+++ main.py
@@ -66,3 +66,15 @@ def delete_post(id: int):
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} does not exist")
     return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    index = find_post_index(id)
+    if index == None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} does not exist")
+    post_dict = post.model_dump()
+    post_dict['id'] = id
+    my_posts[index] = post_dict
+    return {"data": post_dict}

01019720c40e40f0538ca31341ec03852b51ab39
Nathaniel Abayneh <natnaelabayneh5@gmail.com>
2024-12-23 15:28:58 +0300

    get single post by id

--- app/main.py
+++ app/main.py
@@ -71,12 +71,13 @@ def create_post(post: Post):
 
 @app.get("/posts/{id}")
 def get_post(id: int, response: Response):
-    post = find_post(id)
+    cursor.execute("""SELECT * FROM posts WHERE id = %s""", (str(id)))
+
+    post = cursor.fetchone()
+
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id {id} was not found")
-        # response.status_code = status.HTTP_404_NOT_FOUND
-        # return {"message": f"post with id {id} was not found"}
 
     return {"new post": post}
 

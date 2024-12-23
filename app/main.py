1304aa4ea42d55c4c94efcb1f65770a93b18d6c2
Nathaniel Abayneh <natnaelabayneh5@gmail.com>
2024-12-23 20:12:37 +0300

    created table from sqlalchemy

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
 from typing import Optional
-from fastapi import FastAPI, Response, status, HTTPException
+from fastapi import FastAPI, Response, status, HTTPException, Depends
 from pydantic import BaseModel
 from random import randrange
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
+from . import models
+from .database import engine, get_db
+from sqlalchemy.orm import Session
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
@@ -48,6 +53,11 @@ def find_post_index(id):
 @app.get("/")
 async def root():
     return {"message": "welcome to my api!!!"}
+
+
+@app.get("/sqlalchemy")
+def test_posts(db: Session = Depends(get_db)):
+    return {"status": "success"}
 
 
 @app.get("/posts")

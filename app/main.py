e2faabb2760213e91defbdd222127a14c27acaff
Nathaniel Abayneh <natnaelabayneh5@gmail.com>
2024-12-25 11:57:51 +0300

    get all posts using sqlalchemy orm (unpack the dict)

--- app/main.py
+++ app/main.py
@@ -76,8 +76,9 @@ def create_post(post: Post, db: Session = Depends(get_db)):
     # new_post = cursor.fetchone()
 
     # conn.commit()
-    new_post = models.Post(
-        title=post.title, content=post.content, published=post.published)
+    # new_post = models.Post(
+    #     title=post.title, content=post.content, published=post.published)
+    new_post = models.Post(**post.model_dump())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)

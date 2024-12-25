3a20081cacd69158a2c99285c06a99abf31176ea
Nathaniel Abayneh <natnaelabayneh5@gmail.com>
2024-12-25 11:50:03 +0300

    create post using sqlalchemy orm

--- app/main.py
+++ app/main.py
@@ -69,14 +69,18 @@ def get_posts():
 
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_post(post: Post):
-    cursor.execute(
-        """ INSERT INTO posts (title, content, published) VALUES(%s, %s, %s) RETURNING * """, (post.title, post.content, post.published))
+def create_post(post: Post, db: Session = Depends(get_db)):
+    # cursor.execute(
+    #     """ INSERT INTO posts (title, content, published) VALUES(%s, %s, %s) RETURNING * """, (post.title, post.content, post.published))
 
-    new_post = cursor.fetchone()
+    # new_post = cursor.fetchone()
 
-    conn.commit()
-
+    # conn.commit()
+    new_post = models.Post(
+        title=post.title, content=post.content, published=post.published)
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
     return {"new_post": new_post}
 
 
